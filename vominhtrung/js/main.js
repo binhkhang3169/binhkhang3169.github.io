590b5261b561b9888e9acf01e055d48708f6b51f
binhkhang3169 <100997466+binhkhang3169@users.noreply.github.com>
2022-08-23 17:42:12 +0700

    Add files via upload

---
+++ vominhtrung/js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+
+ $('#boy').click(function() {
+ 	$("#boy").removeAttr("click");
+ 	$('#say').text("Hôm nay là ngày cc gì ấy nhỉ？");
+ 	$('#boy').click(function() {
+ 		$("#boy").removeAttr("click");
+ 		$('#say').text("Ò I ấn đơ tút？");
+ 		$('#boy').click(function() {
+ 			$("#boy").removeAttr("click");
+ 			$('#say').text("Thì ra là hôm nay.");
+ 			$('#boy').click(function() {
+ 				$("#boy").removeAttr("click");
+ 				$('#say').text("Thì ra là một ngày vón đờ phùn.");
+	 			$('#boy').click(function() {
+	 				$("#boy").removeAttr("click");
+	 				$('#say').text("Và đó là....");
+	 				$('#boy').click(function() {
+	 					$("#boy").removeAttr("click");
+		 				$('#say').text("Một ngày ai ấu sì mà.");
+		 				$('#boy').click(function() {
+		 					$("#boy").removeAttr("click");
+			 				$('#say').text("Ngày một huyền thoại ra đời.");
+			 				$('#boy').click(function() {
+			 					$("#boy").removeAttr("click");
+				 				$('#say').text("its Valkin！");
+				 				$('#boy').click(function() {
+				 					$("#boy").removeAttr("click");
+					 				$('#say').text("Vankinnnn.");
+					 				$('#boy').click(function() {
+					 					$("#boy").removeAttr("click");
+						 				$('#say').text("Happy Buscu!!!!!");
+						 					$('#boy').attr('src', 'image/02.gif')
+						 					click1();
+						 			});
+					 			});
+				 			});
+			 			});
+		 			});
+	 			});
+ 			});
+ 		});
+ 	});
+ });
+
+
+
+function click1() {
+	$('#boy').click(function() {
+		$("#boy").removeAttr("click");
+		$('#say').text("( •̀ ω •́ )！！！");
+		$('#boy').click(function() {
+			$("#boy").removeAttr("click");
+			$('#say').text("Chúc Valkin bớt ảo tưởng sức mạnh lại♪(^∇^*)！");
+			$('#boy').click(function() {
+				$("#boy").removeAttr("click");
+				$('#say').text("(/≧▽≦)/Chúc càng ngày càng đá banh giỏi");
+				$('#boy').click(function() {
+					$("#boy").removeAttr("click");
+					$('#say').text("Ài ấu sì mà！");
+				});
+			});
+		});
+	});
+}
+
+
+});
